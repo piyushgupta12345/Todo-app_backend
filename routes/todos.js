a3e9c2cde0dc76b5c19fa73f32e8c8d793696ed4
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-09-20 23:57:07 +0530

    update

--- routes/todos.js
+++ routes/todos.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 // import controller
 const {createTodo} = require("../controllers/createTodo")
+const {getTodo,getTodoByID} = require("../controllers/getTodo")
 
 // define Api routes
 router.post('/createTodo', createTodo);
+router.get('/getTodo', getTodo);
+router.get('/getTodo/:id', getTodoByID);
 
 module.exports = router;
